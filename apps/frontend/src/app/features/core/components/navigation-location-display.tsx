b70a53e16e28ba61217eebb5cc26b73873ced6b0
GLima2006 <111139925+GLima2006@users.noreply.github.com>
2023-09-21 15:04:11 -0300

    feat: add className to navigation location display

--- apps/frontend/src/app/features/core/components/navigation-location-display.tsx
+++ apps/frontend/src/app/features/core/components/navigation-location-display.tsx
@@ -3,16 +3,21 @@ import Row from "./row";
 import Text from "./text";
 import HouseIcon from "@assets/icons/house.svg";
 import ChevronRightIcon from "@assets/icons/chevron-right.svg";
+import { classNameBuilder } from "@/helpers/class-name-builder";
 
-export interface NavigationLocationDisplayProps {}
-const NavigationLocationDisplay: React.FC<
-  NavigationLocationDisplayProps
-> = ({}) => {
+export interface NavigationLocationDisplayProps {
+  className?: string;
+}
+const NavigationLocationDisplay: React.FC<NavigationLocationDisplayProps> = ({
+  className,
+}) => {
   return (
-    <Row className="gap-x-2 items-center">
+    <Row className={classNameBuilder("gap-x-2 items-center", className)}>
       <HouseIcon className="text-gray-default" />
       <ChevronRightIcon className="h-5 w-5 text-gray-default" />
-      <Text color="default-gray" size="sm" bold>Todos os produtos</Text>
+      <Text color="default-gray" size="sm" bold>
+        Todos os produtos
+      </Text>
     </Row>
   );
 };
