63541f19ea9f699cb82f2b3c4afe5538973ef561
GLima2006 <111139925+GLima2006@users.noreply.github.com>
2023-09-21 17:25:32 -0300

    feat: webpage component container

--- apps/frontend/src/app/features/core/components/webpage.tsx
+++ apps/frontend/src/app/features/core/components/webpage.tsx
@@ -1,17 +1,31 @@
 import React from "react";
 import Column from "./column";
 import { classNameBuilder } from "@/helpers/class-name-builder";
+import Header from "./header";
+import Footer from "./footer";
 
 export interface WebpageProps {
+  header?: React.ReactNode;
+  footer?: React.ReactNode;
   children?: React.ReactNode;
   className?: string;
 }
-const Webpage: React.FC<WebpageProps> = ({ className, children }) => {
+const Webpage: React.FC<WebpageProps> = ({
+  header,
+  footer,
+  className,
+  children,
+}) => {
   return (
     <Column
-      className={classNameBuilder("bg-[#EEEEEE] min-w-full min-h-screen", className)}
+      className={classNameBuilder(
+        "bg-[#EEEEEE] min-w-full min-h-screen",
+        className
+      )}
     >
-      {children}
+      {header ?? <Header />}
+      <Column className="w-full h-full max-w-[1440px] mx-auto">{children}</Column>
+      {footer ?? <Footer />}
     </Column>
   );
 };
